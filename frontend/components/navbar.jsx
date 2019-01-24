2752538df98ff659248bb386840ecb524ca540ad
Michael Cho <michaelhcho91@gmail.com>
2019-01-24 17:53:06 -0500

    Style navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
+  // const upload_icon = require("../../app/assets/images/icons/upload_icon.png");
 
   render() {
     return (
-      <nav>
-
+      <nav className="nav-container">
+        <div className="nav-list-container">
+          <ul className="nav-list">
+            <li className="nav-left-items">
+              <div><Link to={`/`} className="icon-feed"><img src={window.feed_icon}/></Link></div>
+              <div className="nav-divider"></div>
+              <div><Link to={`/`} className="nav-logo">Finstagram</Link></div>
+            </li>
+            <li className="nav-search">
+              <input type="text" placeholder="Search"/>
+            </li>
+            <li className="nav-right-items">
+              <Link to={`/`} className="icon-upload"><img src={window.upload_icon}/></Link>
+              <Link to={`/`} className="icon-heart"><img src={window.heart_icon}/></Link>
+              <Link to={`/`} className="icon-profile"><img src={window.profile_icon}/></Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     )
   }
--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostIndexItem from "./post_index_item";
+import Navbar from "../navbar";
 
 class PostIndex extends React.Component {
   componentDidMount() {
@@ -13,7 +14,8 @@ class PostIndex extends React.Component {
 
     return (
       <>
-        <ul>
+        <Navbar />
+        <ul className="posts-container">
           {posts}
           <button onClick={this.props.logout}>Logout</button>
         </ul>  
