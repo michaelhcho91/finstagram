e01a9a2ffe31ed2a14b2635f8819603c91f4c466
Michael Cho <michaelhcho91@gmail.com>
2019-01-24 20:27:17 -0500

    Adjust auth errors

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,7 +6,7 @@ class Api::UsersController < ApplicationController
       login(@user)
       render "api/posts/feed"
     else
-      render json: ["Email, username, and password required."], status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end   
 
