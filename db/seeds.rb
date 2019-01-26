c571d5822e3c26e449d584397faac1483c519456
Michael Cho <michaelhcho91@gmail.com>
2019-01-26 00:27:57 -0500

    Edit configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,3 @@ user1 = User.create({ username: "michael", email: "michael@email.com", password:
 user2 = User.create({ username: "richard", email: "richard@email.com", password: "password"})
 user3 = User.create({ username: "uriah", email: "uriah@email.com", password: "password"})
 user4 = User.create({ username: "grace", email: "grace@email.com", password: "password"})
-
-Post.create({ poster_id: user1.id, caption: "post 1 test" })
-Post.create({ poster_id: user2.id, caption: "post 2 test" })
-Post.create({ poster_id: user3.id, caption: "post 3 test" })
