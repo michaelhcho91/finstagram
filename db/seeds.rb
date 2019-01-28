b886e6ad91a583a130378446541f411ba78daec2
Michael Cho <michaelhcho91@gmail.com>
2019-01-28 14:50:02 -0500

    Reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,15 @@
 User.destroy_all
 Post.destroy_all
 
-User.create({ username: "demo", email: "demo@demo.com", password: "password", name: "Demo User"})
+demo = User.create({ username: "demo", email: "demo@demo.com", password: "password", name: "Demo User"})
+demo.photo.attach(io: File.open("#{Rails.root}/app/assets/images/session/default_profile.jpg"), filename: "default_profile.jpg")
 
 user1 = User.create({ username: "michael", name: "", email: "michael@email.com", password: "password" })
 user2 = User.create({ username: "richard", name: "", email: "richard@email.com", password: "password" })
 user3 = User.create({ username: "uriah", name: "", email: "uriah@email.com", password: "password" })
 user4 = User.create({ username: "grace", name: "", email: "grace@email.com", password: "password" })
+
+user1.photo.attach(io: File.open("#{Rails.root}/app/assets/images/session/default_profile.jpg"), filename: "default_profile.jpg")
+user2.photo.attach(io: File.open("#{Rails.root}/app/assets/images/session/default_profile.jpg"), filename: "default_profile.jpg")
+user3.photo.attach(io: File.open("#{Rails.root}/app/assets/images/session/default_profile.jpg"), filename: "default_profile.jpg")
+user4.photo.attach(io: File.open("#{Rails.root}/app/assets/images/session/default_profile.jpg"), filename: "default_profile.jpg")
